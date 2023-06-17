e1c4bb156122db13079868d7552f39b2ca250f70
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-17 04:49:03 +0000

    remove tabs

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,39 +9,38 @@
 
 int main(void)
 {
-	int hun;
-	int unit;
-	int ten;
-
-	for (hun = 0; hun < 10; hun++)
-	{
-		for (ten = 0; ten < 10; ten++)
-		{
-			if (ten != hun)
-			{
-				if (ten > hun)
-				{
-					for (unit = 0; unit < 10; unit++)
-					{
-						if (ten != unit)
-							{
-								if (unit > ten)
-								{
-									putchar(48 + hun);
-									putchar(48 + ten);
-									putchar(48 + unit);
-									if (hun != 7 || ten != 8 || unit != 9)
-									{
-										putchar(',');
-										putchar(' ');
-									}
-								}
-							}
-					}
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int hun;
+int unit;
+int ten;
+for (hun = 0; hun < 10; hun++)
+{
+for (ten = 0; ten < 10; ten++)
+{
+if (ten != hun)
+{
+if (ten > hun)
+{
+for (unit = 0; unit < 10; unit++)
+{
+if (ten != unit)
+{
+if (unit > ten)
+{
+putchar(48 + hun);
+putchar(48 + ten);
+putchar(48 + unit);
+if (hun != 7 || ten != 8 || unit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
