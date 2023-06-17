05766d74e526e72eac13041832d8d3c836987f2b
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-17 04:28:00 +0000

    print combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,29 +9,39 @@
 
 int main(void)
 {
+	int hun;
 	int unit;
 	int ten;
 
-	for (ten = 0; ten < 10; ten++)
+	for (hun = 0; hun < 10; hun++)
 	{
-		for (unit = 0; unit < 10; unit++)
+		for (ten = 0; ten < 10; ten++)
 		{
-			if (ten != unit)
+			if (ten != hun)
 			{
-				if (ten <= 8 && unit <= 9)
+				if (ten > hun)
 				{
-				putchar(48 + ten);
-				putchar(48 + unit);
-				if (ten != 8 || unit != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+					for (unit = 0; unit < 10; unit++)
+					{
+						if (ten != unit)
+							{
+								if (unit > ten)
+								{
+									putchar(48 + hun);
+									putchar(48 + ten);
+									putchar(48 + unit);
+									if (hun != 7 || ten != 8 || unit != 9)
+									{
+										putchar(',');
+										putchar(' ');
+									}
+								}
+							}
+					}
 				}
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
