239ba5f622949b2a7a366b0031275f516cf6fb81
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-17 01:18:51 +0000

    count alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
