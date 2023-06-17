1f05305389a90c75dedf0503cd0875a1751f4b5b
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-17 01:14:58 +0000

    print numbers as alphabets

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+	{
+		putchar(48 + dig);
+	}
+	putchar('\n');
+	return (0);
+}
