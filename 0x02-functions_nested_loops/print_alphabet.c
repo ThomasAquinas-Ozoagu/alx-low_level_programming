2d74364143e74f7933abccde9e36c8856467d8ac
Ozoagu <ozoagu.benedict@gmail.com>
2023-06-21 20:38:02 +0000

    print a-z 9 more times

---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,17 @@
+/**
+ * print_alphabet - print a-z.
+ *
+ * Return: Always 0.
+ */
+
+
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = a; alpha <= z; alpha++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
