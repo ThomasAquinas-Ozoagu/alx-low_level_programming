2e60bae3825c94a1bbe5efeb6bad0c8729250fdc
Ozoagu <ozoagu.benedict@gmail.com>
2023-06-25 17:57:48 +0000

    use 60 instead 6 and 0

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,21 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int b, c;
 
-	for (a = 0; a < 3; a++)
+
+
+	for (b = 0; b < 24; b++)
 	{
-	for (b = 0; b < 4; b++)
+	for (c = 0; c < 60; c++)
 	{
-	for (c = 0; c < 6; c++)
-	{
-	for (d = 0; d < 10; d++)
-	{
-		_putchar('0' + a);
-		_putchar('0' + b);
+		_putchar('0' + (b / 10));
+		_putchar('0' + (b % 10));
 		_putchar(':');
-		_putchar('0' + c);
-		_putchar('0' + d);
+		_putchar('0' + (c / 10));
+		_putchar('0' + (c % 10));
 		_putchar('\n');
-	}
-	}
+
 	}
 	}
 
