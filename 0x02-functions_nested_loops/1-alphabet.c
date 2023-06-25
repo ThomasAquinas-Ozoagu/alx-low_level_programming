6022b2f03ac01211119778cef5f1f2bac8a98aff
Ozoagu <ozoagu.benedict@gmail.com>
2023-06-25 14:55:53 +0000

    remove head

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-=======
-
 /**
  * print_alphabet - Displays all the aphabets in lower case when called
  *
@@ -20,12 +8,10 @@ int main(void)
 
 void print_alphabet(void)
 {
-	char alpha;
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	for (char alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
 	}
 	_putchar('\n');
->>>>>>> 5f92fcc8ff754525262ad4537fdb962b9c46200b
 }
