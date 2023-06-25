2b663efe4166bfb66c450e66ab2da1bb39433ef1
Ozoagu <ozoagu.benedict@gmail.com>
2023-06-25 20:06:14 +0000

    just add the numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @n: the first of the two numbers to be added
+ * @m: the second of the two numbers to be added
+ * Return: result of the arithmetics.
+ */
+int add(int m, int n)
+{
+	return (m + n);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -36,3 +36,5 @@ int print_last_digit(int);
 void jack_bauer(void);
 
 void times_table(void);
+
+int add(int, int);
