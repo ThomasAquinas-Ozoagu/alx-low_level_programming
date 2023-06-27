5dfc0725288d7cd53e2398db2f8cb110011ce6e4
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-27 23:46:26 +0000

    modify do while loop

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,14 +14,26 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (i = 0; src[i] != '\0'; i++)
-		*(dest + i) = src[i];
-
-
-/*   do { */
+/*	for (i = 0; src[i] != '\0'; i++)*/
 /*		*(dest + i) = src[i];*/
-/*		i++;*/
-/*	} while (src[i] != '\0');*/
+
+
+	do {
+		if (src[i] == '\0')
+		{
+			*(dest + i) = src[i];
+			break;
+		}
+		else
+		{
+			*(dest + i) = src[i];
+			i++;
+			if (src[i] == '\0')
+			{
+				*(dest + i) = src[i];
+			}
+		}
+	} while (src[i] != '\0');
 
 	return (dest);
 }
