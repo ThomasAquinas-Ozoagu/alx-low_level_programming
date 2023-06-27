7c91251d642b235ab9ed9652ef8964628eaeb0a2
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-06-27 03:07:29 +0000

    print last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * puts_half - prints the second half of a string
+ *
+ * @str: the string whose lenght is about to be measured
+ *
+ * Return: the integer result of the analysis.
+ */
+
+void puts_half(char *str)
+{
+	int l, m, n;
+	int count = 0;
+
+	for (n = 0; str[n] != '\0'; n++)
+		count++;
+
+	if (count % 2 != 0)
+		l = (count + 1) / 2;
+	else
+		l = count / 2;
+
+	for (m = l; m <= count; m++)
+		_putchar(str[m]);
+
+	_putchar('\n');
+}
