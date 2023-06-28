3b0a1d17bf2d48fb4b050fb0c8cf8afcdaeec25b
Ozoagu <ozoagu.benedict@gmail.com>
2023-06-28 20:12:22 +0000

    multiple both integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: integer 1
+ *
+ * @b: integer 2
+ *
+ * Return: Result of the multiplication.
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
