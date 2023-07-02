5cc25250539ca6c81803db48d0ad38114a2e281d
Ozoagu <ozoagu.benedict@gmail.com>
2023-07-02 00:47:42 +0000

    join those strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strncpy - that copies a string src to dest starting from n
+ *
+ * @dest: base string
+ *
+ * @src: string to be appended
+ *
+ * @n: The number of src strings to be used
+ *
+ * Return: a pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+/*	for (j = 0; dest[j] != '\0'; j++)*/
+
+/*	for (k = 0; src[k] != '\n'; k++)*/
+
+
+/*	if (k > n)*/
+
+		for (i = 0; i < n ; i++)
+		{
+			*(dest + i) = src[i];
+		}
+
+
+	return (dest);
+}
