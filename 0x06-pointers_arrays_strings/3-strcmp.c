aedbf524009b56eb7d2d8ad7a01418b11ea47e6f
Ozoagu <ozoagu.benedict@gmail.com>
2023-07-02 01:41:55 +0000

    check if two strings are equal

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - checks two strings for equal number of bytes
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: integer 0 if equal, +15 if 1 > 2 and -15 if 2 > 1.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int j, k;
+
+	for (j = 0; s1[j] != '\0'; j++)
+		;
+
+	for (k = 0; s2[k] != '\0'; k++)
+		;
+
+	if (j > k)
+		return (15);
+	if (j < k)
+		return (-15);
+	else
+		return (0);
+}
