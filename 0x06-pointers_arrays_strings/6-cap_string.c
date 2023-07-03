6142d77d2a3be537611941e698c1e72e2458e254
Ozoagu <ozoagu.benedict@gmail.com>
2023-07-03 03:53:43 +0000

    continue

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: the string to be modified.
+ *
+ * Return: the modified string.
+ *
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+	alpha = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((i == 0) && ((str[i] > 96 && str[i] < 123))
+			str[i] -= ' ';
+
+		if (str[i]
+	}
+}
