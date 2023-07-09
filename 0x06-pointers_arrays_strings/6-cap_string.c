7828c02b689d97a56a8e0ca005771f17c0ab8af5
Ozoagu <ozoagu.benedict@gmail.com>
2023-07-09 21:48:58 +0000

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,30 @@
 char *cap_string(char *str)
 {
 	int i;
-	alpha = 0;
+	int alpha = 0;
 
 	for (i = 0; str[i]; i++)
 	{
-		if ((i == 0) && ((str[i] > 96 && str[i] < 123))
-			str[i] -= ' ';
-
-		if (str[i]
+		if ((i == 0) && ((str[i] > 96 && str[i] < 123)))
+			{
+				str[i] -= ' ';
+			}
+			else if (alpha == 1)
+			{
+				if (str[i] > 96 && str[i] < 123)
+					str[i] -= ' ';
+				alpha = 0;
+			}
+			else if (str[i] == 9 || str[i] == 10 || str[i] == 32)
+				alpha = 1;
+			else if (str[i] == 33 || str[i] == 40 || str[i] == 41)
+				alpha = 1;
+			else if (str[i] == 44 || str[i] == 46 || str[i] == 59)
+				alpha = 1;
+			else if (str[i] == 63 || str[i] == 123 || str[i] == 125)
+				alpha = 1;
+			else if (str[i] == 34)
+				alpha = 1;
 	}
+	return (str);
 }
