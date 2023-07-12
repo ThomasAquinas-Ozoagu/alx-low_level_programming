91b949387ce3a83f0f6651fe6414ed9b2978d5a4
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-12 02:35:58 +0000

    do not allow NULL input

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,6 +17,12 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	int m = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i]; i++)
 		;
 	for (j = 0; s2[j]; j++)
