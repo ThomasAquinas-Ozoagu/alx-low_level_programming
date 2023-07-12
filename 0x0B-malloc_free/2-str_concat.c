f29dfcfcd55d40a8e2c83917e3ef29928ff047ec
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-12 02:32:22 +0000

    combine them

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+/**
+ * str_concat - function that concatenates two strings.
+ *
+ * @s1: string 1
+ *
+ * @s2: string 2
+ *
+ * Return: pointer to the result
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, l;
+	char *s3;
+	int m = 0;
+
+	for (i = 0; s1[i]; i++)
+		;
+	for (j = 0; s2[j]; j++)
+		;
+
+	s3 = malloc((i + j) * sizeof(char));
+
+	for (k = 0; k < i; k++)
+		s3[k] = s1[k];
+
+	for (l = 0; l < j; l++)
+	{
+		s3[k + l] = s2[l];
+		if (s3[k + l] != s2[l])
+			m = 1;
+	}
+
+	s3[i + j] = '\0';
+
+	if (m > 0)
+		return (NULL);
+
+	return (s3);
+}
