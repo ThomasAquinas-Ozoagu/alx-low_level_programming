1dd6f9fa8012877e168ef03757515caef18e69e1
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-13 04:20:49 +0000

    free those memory slots

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -27,7 +27,6 @@ void print_grid(int **grid, int width, int height)
 		printf("\n");
 		h++;
 	}
-
 }
 
 /**
@@ -49,6 +48,6 @@ int main(void)
 	grid[0][3] = 98;
 	grid[3][4] = 402;
 	print_grid(grid, 6, 4);
-	free(grid);
+	free_grid(grid, 4);
 	return (0);
 }
