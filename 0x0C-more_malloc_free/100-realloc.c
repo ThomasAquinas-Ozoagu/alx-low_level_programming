f5fdca8d85dd4c723192ba4c44b32e50f0b53893
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-14 22:09:38 +0000

    check all malloc calls

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,7 +23,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 
 	if (!ptr)
-		return (malloc(new_size));
+	{
+		ptr = malloc(new_size);
+		if (!ptr)
+			return (NULL);
+		else
+			return (ptr);
+	}
 
 	if (!new_size && pt)
 	{
