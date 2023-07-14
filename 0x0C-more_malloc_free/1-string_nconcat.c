70b4c795530ffc567143f813cd9b9641e9cf24c9
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-14 23:19:28 +0000

    use s1 + s2 for malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > i)
 		n = i;
 
-	comb = malloc(sizeof(s1) * (m + n));
+	comb = malloc(sizeof(s1) + sizeof(s2));
 	if (!comb)
 		return (NULL);
 
