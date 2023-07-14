ce9b9f3df1efba4b6f5254a8a1da90f5670f7cc6
Ozoagu <ozoagu.benedict@gmail.com>
2023-07-14 14:33:49 +0000

    stop wasting time

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -23,7 +23,7 @@ int *array_range(int min, int max)
 	if (!nums)
 		return (NULL);
 
-	for (i = 0; i <= (max - min + 1); i++)
+	for (i = 0; i <= (max - min); i++)
 		nums[i] = min + i;
 
 	return (nums);
