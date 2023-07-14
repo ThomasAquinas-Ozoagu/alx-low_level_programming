9eb05b95884267e51e7164dcc88958782a9c45da
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2023-07-14 23:13:37 +0000

    use one char instead of an entire string

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiplies two positive numbers
+ *
+ * @argc: number of inputs to be multiplied
+ *
+ * @argv: the numbers to be multiplied
+ *
+ * Return: 0 if everything works, 98 if an error occurs.
+ */
+
+int main(int argc, char *argv[])
+{
+
+/*	int i, j, k; */
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+
+	return (0);
+}
