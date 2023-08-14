d56e46a6ecc7426b876d3598ffaa408228219fb5
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-14 23:49:32 +0000

    do not be afraid of smaller functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,13 @@
 #include "main.h"
+void print_100(int num);
+void print_10(int num);
+void print_1(int num);
+
 /**
  * print_to_98 -  prints all natural numbers from n to 98, followed by \n.
  * @m: - the number where the printing starts
  */
+
 void print_to_98(int m)
 {
 	int n;
@@ -10,59 +15,70 @@ void print_to_98(int m)
 	if (m < 98)
 		for (n = m; n < 98; n++)
 		{
-			if (n < 0)
+			if (n < -0)
 			{
 				_putchar('-');
-
-				if (n < -9)
-				  {
-				    _putchar('0' + (n / -10));
-				    _putchar('0' + (n % -10));
-				  }
+				if (n < -99)
+					print_100(-n);
+				else if (n > -100 && n < -9)
+					print_10(-n);
 				else
-				  {
-				    _putchar('0' + (-n));
-				  }
+					print_1(-n);
 			}
-			else if (n > 10)
-			  {
-			    _putchar('0' + (n / 10));
-			    _putchar('0' + (n % 10));
-			  }
+			else if (n >= 0 && n < 10)
+				print_1(n);
 			else
-			  {
-			    _putchar('0' + (n % 10));
-			  }
-
+				print_10(n);
 			_putchar(',');
 			_putchar(' ');
 		}
-
-
 	if (m > 98)
 	{
 		for (n = m; n > 98; n--)
 		{
 			if (n > 99)
-			  {
-			    _putchar('0' + (n / 100));
-			    _putchar('0' + ((n % 100) / 10));
-			    _putchar('0' + ((n % 100) % 10));
-			  }
-			else if ((n < 100) && (n > 10))
-			  {
-			    _putchar('0' + (n / 10));
-			    _putchar('0' + (n % 10));
-			  }
+				print_100(n);
+			else if ((n < 100) && (n > 9))
+				print_10(n);
 			else
-			  {
-			    _putchar('0' + (n % 10));
-			  }
+				print_1(n);
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
-	_putchar('9');
-	_putchar('8');
+	print_10(98);
 	_putchar('\n');
+}
+
+/**
+ * print_100 - displays 3 digit numbers
+ *
+ * @num: number to be displayed
+ */
+void print_100(int num)
+{
+	_putchar('0' + (num / 100));
+	_putchar('0' + ((num % 100) / 10));
+	_putchar('0' + ((num % 100) % 10));
+}
+
+/**
+ * print_10 - displays 2 digit numbers
+ *
+ * @num: number to be displayed
+ */
+void print_10(int num)
+{
+	_putchar('0' + (num / 10));
+	_putchar('0' + (num % 10));
+}
+
+/**
+ * print_1 - displays 1 digit number
+ *
+ * @num: number to be displayed
+ */
+void print_1(int num)
+{
+	_putchar('0' + num);
 }
