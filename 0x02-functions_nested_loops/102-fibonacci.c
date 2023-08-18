ca0a7577fe5f3a3f808dcaf566f5e17badfccbc5
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-18 07:47:31 +0000

    always pay attention to the format printing

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	unsigned long int n = 0, m = 1, sum;
+	unsigned long int n = 1, m = 1, sum;
 	int count;
 
-
-	for (count = 0; count < 50; count++)
+	printf("1");
+	for (count = 0; count < 49; count++)
 	{
 		sum = n + m;
 		n = m;
 		m = sum;
 
-		printf("%lu, ", sum);
+		printf(", %lu", sum);
 	}
 	printf("\n");
 	return (0);
