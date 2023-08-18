40ed3c96161e3fb1f90c627f136aaf2e8ce963dd
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-18 07:39:43 +0000

    always initialize your variables

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0 if ok
+ */
+
+int main(void)
+{
+	unsigned long int n, m, sum, total = 0;
+
+
+	n = 0;
+	m = 1;
+
+	while (1)
+	{
+		sum = n + m;
+		n = m;
+		m = sum;
+
+		if (sum < 4000000)
+		{
+			if (sum % 2 == 0)
+			{
+				total += sum;
+				printf("%lu, ", total);
+			}
+
+		}
+		else
+			break;
+	}
+	printf("\n");
+	return (0);
+}
