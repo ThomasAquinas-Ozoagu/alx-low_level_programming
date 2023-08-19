e01155021431140aa8efd3dc7977e2cbc4e58807
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-19 16:54:21 +0000

    stop trying to solve all the problems at once

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-unsigned long int print_long(unsigned long int num_a, unsigned long int num_b);
-
 
 /**
  * main - prints the first 50 Fibonacci numbers
@@ -43,31 +41,4 @@ int main(void)
 	}
 	printf("\n");
 	return (0);
-}
-
-/**
- * print_long - adds numbers longer than 15 digits without overflowing
- *
- * @num_a: first number
- *
- * @num_b: Second number
- *
- * Return: sum of the two numbers
- */
-
-unsigned long int print_long(unsigned long int num_a, unsigned long int num_b)
-{
-	unsigned long int lsb_a = num_a % 1000000000000000;
-	unsigned long int msb_a = num_a / 1000000000000000;
-	unsigned long int lsb_b = num_b % 1000000000000000;
-	unsigned long int msb_b = num_b / 1000000000000000;
-	unsigned long int lsb_sum = lsb_a + lsb_b;
-	unsigned long int rem = lsb_sum / 1000000000000000;
-	unsigned long int msb_sum = rem + msb_a + msb_b;
-
-	printf(", ");
-	if (msb_sum > 0)
-		printf("%lu", msb_sum);
-	printf("%lu", lsb_sum);
-	return (num_a + num_b);
 }
