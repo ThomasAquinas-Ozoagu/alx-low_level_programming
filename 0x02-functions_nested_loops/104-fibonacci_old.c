7960edb59538117d46d7677eebd5305f6eff6477
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-19 16:52:54 +0000

    stop trying to solve all the problems at once

--- 0x02-functions_nested_loops/104-fibonacci_old.c
+++ 0x02-functions_nested_loops/104-fibonacci_old.c
@@ -11,35 +11,18 @@ unsigned long int print_long(unsigned long int num_a, unsigned long int num_b);
 
 int main(void)
 {
-	unsigned long int n = 1, y = 1, sum1, sum2 = 0, m = 0, x = 0, rem = 0;
+	unsigned long int n = 1, m = 1, sum;
 	int count;
 
 	printf("1");
 	for (count = 0; count < 97; count++)
 	{
-		if (n > 1000000000000000)
-		{
-			m = n / 1000000000000000;
-			n = n % 1000000000000000;
-			x = y / 1000000000000000;
-			y = y % 1000000000000000;
-		}
-		sum1 = (n + y);
-		rem = sum1 / 1000000000000000;
-		sum1 = sum1 % 1000000000000000;
-		sum2 = rem + m + x;
-		x = m;
-		y = n;
+		sum = print_long(n, m);
+		n = m;
+		m = sum;
+		
 
-		m = sum2;
-		n = sum1;
-
-
-		printf(", ");
-		if (sum2 > 0)
-			printf("%lu", sum2);
-		printf("%lu", sum1);
-
+/*		printf(", %lu", sum); */
 	}
 	printf("\n");
 	return (0);
