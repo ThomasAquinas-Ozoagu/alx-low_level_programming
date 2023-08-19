4f8129183aea161171eba1c45c0f6d4bac0287af
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-08-19 14:43:55 +0000

    learn more about data types

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers
+ *
+ * Return: 0 if ok
+ */
+
+int main(void)
+{
+	unsigned long int n = 1, m = 1, sum;
+	int count;
+
+	printf("1");
+	for (count = 0; count < 97; count++)
+	{
+		sum = n + m;
+		n = m;
+		m = sum;
+
+		printf(", %lu", sum);
+	}
+	printf("\n");
+	return (0);
+}
