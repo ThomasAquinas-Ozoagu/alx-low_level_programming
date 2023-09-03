a77d179fc8523d4e051bcb4ec8fbec6f1f03b844
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-09-03 09:27:43 +0000

    find out how to use an argument that I do not need in other to satisfy Betty

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the program name
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: 0 if OK
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argv[0])
+	{	printf("%d\n", argc);
+		break;
+	}
+	return (0);
+}
