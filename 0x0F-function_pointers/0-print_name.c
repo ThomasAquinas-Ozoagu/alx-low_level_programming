630336559d1264d04c54f7a7dfeca9510a218f50
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-09-14 23:46:07 +0000

    doing is different from knowing, never forget

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name: invokes a function that prints a name
+ *
+ * @name: The name to be printed
+ *
+ * @f: A function pointer
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+Even Functions can have pointers.
+This directory is evidence of such practice in my custody.
