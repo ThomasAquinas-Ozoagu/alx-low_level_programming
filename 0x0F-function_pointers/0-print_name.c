2d670536e253dfb70c1a87fd3a3ce5d5795f95f9
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-09-14 23:48:20 +0000

    do not mess around with Betty

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name: invokes a function that prints a name
+ * print_name - invokes a function that prints a name
  *
  * @name: The name to be printed
  *
