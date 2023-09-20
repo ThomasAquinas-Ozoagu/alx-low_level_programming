6ee16c81c3635a58a3a97045a2e6901b9448b9b2
OZOAGU, ThomasAquinas <ozoagu.benedict@gmail.com>
2023-09-20 00:23:43 +0000

    you have to give in the time

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains my exercise files on functions that can take multiple variables.
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,15 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#ifndef _PUTCHAR_
+#define _PUTCHAR_
+int _putchar(char c);
+#endif /* _PUTCHAR_ */
+
+#ifndef _SUM_THEM_ALL_
+#define _SUM_THEM_ALL_
+int sum_them_all(const unsigned int n, ...);
+#endif /* _SUM_THEM_ALL_ */
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
+
