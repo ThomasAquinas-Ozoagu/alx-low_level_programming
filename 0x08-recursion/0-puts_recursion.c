41a921a1f54df30a57903961e7a6c44d4cf06e20
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-10-28 13:49:30 +0000

    find out how to untrack a file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ *
+ * @s: The string to be printed
+ *
+ * Return: nothing
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if (s[0])
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
