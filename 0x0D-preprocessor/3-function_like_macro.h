1df4c764885af6308a4f70b5c8988b1eb6bd70da
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-06 17:59:02 +0000

    check the header file

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,4 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
-
 #ifndef ABS(x)
 #define ABS(x) (((x) > 1) ? (x) : (-1 * (x)))
 #endif /* ABS */
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
