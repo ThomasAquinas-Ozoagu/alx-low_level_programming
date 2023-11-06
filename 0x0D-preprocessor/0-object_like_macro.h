b2e80dde2f0f2de680eee5962ac095286cb28fa1
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-06 18:02:02 +0000

    look out for recent changes

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,5 @@
-#ifndef _0-OBJECT_LIKE_MACRO_
-#define _0-OBJECT_LIKE_MACRO_
-
-#ifndef _SIZE_
+#ifndef SIZE
 
 #define SIZE 1024
 
-#endif /* ifndef _SIZE_ */
-
-#endif /* _0-OBJECT_LIKE_MACRO_ */
+#endif /* ifndef SIZE */
