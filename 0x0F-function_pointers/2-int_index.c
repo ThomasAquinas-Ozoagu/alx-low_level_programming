1fa7c4ab45bc55613fb407cc5cfeac713d636730
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-08 23:25:08 +0000

    betty is not your friend, but she is here to help

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,16 +5,18 @@
  * @array: the haystack
  * @size: the number of elements in the array
  * @cmp: a pointer to the function to be used to compare values
- */
+ * Return: index of the first match
+*/
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if ((*cmp)(array[i]) > 0)
+			if ((*cmp)(array[i]) != 0)
 				return (i);
 		}
 	}
