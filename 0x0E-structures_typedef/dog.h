60b134116dc40a315c2ee22ddfa6a19936efa14d
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-08 00:51:34 +0000

    do not complicate the problem, step back a little and try again.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,37 @@ struct dog
 	float age;
 	char *owner;
 
-}
+};
 
 #ifndef _INIT_DOG_
 #define _INIT_DOG_
 	void init_dog(struct dog *d, char *name, float age, char *owner);
-
 #endif /* define _INIT_DOG_ */
+
+#ifndef _PRINT_DOG_
+#define _PRINT_DOG_
+void print_dog(struct dog *d);
+#endif /* void print_dog(struct dog *d) */
+
+
+#ifndef _DOG_T_
+#define _DOG_T_
+typedef struct dog dog_t;
+#endif /* define _DOG_T_ */
+
+#ifndef _NEW_DOG_
+#define _NEW_DOG_
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /* _NEW_DOG_ */
+
+#ifndef _FREE_DOG_
+#define _FREE_DOG_
+void free_dog(dog_t *d);
+#endif /* define _FREE_DOG_ */
+
+
+
+
+
 
 #endif /* define _DOG */
