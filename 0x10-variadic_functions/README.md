15fc1910c8b40af1e76822ed2040dd96e88a603b
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-09 22:40:51 +0000

    it is time to process arguments

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory contains my practice files for variadic functions.
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,22 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
+
+#ifndef _PUTCHAR_
+#define _PUTCHAR_
+int _putchar(char c);
+#endif /* _PUTCHAR_ */
+
+#ifndef _SUM_THEM_ALL_
+#define _SUM_THEM_ALL_
+int sum_them_all(const unsigned int n, ...);
+#endif /* _SUM_THEM_ALL_ */
+
+#ifndef _PRINT_NUMBERS_
+#define _PRINT_NUMBERS_
+void print_numbers(const char *separator, const unsigned int n, ...);
+#endif /* _PRINT_NUMBERS_ */
+
+
+
+
+#endif /* _VARIADIC_FUNCTIONS_H_ */
