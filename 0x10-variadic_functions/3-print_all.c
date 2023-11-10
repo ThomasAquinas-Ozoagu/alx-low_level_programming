582423d8322b0878278fd9597086c8365e5488ce
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-11-10 22:58:08 +0000

    complete all requirements

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-
-
-/**
- * _strcpy - copies a string using a pointer string 1 --> string 2
- *
- * @dest: string 1
- *
- * @src: string 2
- *
- * Return: the pointer to dest
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	/*for (i = 0; src[i] != '\0'; i++)*/
-	/**(dest + i) = src[i];*/
-
-
-	do {
-		if (src[i] == '\0')
-		{
-			*(dest + i) = src[i];
-			break;
-		}
-		else
-		{
-			*(dest + i) = src[i];
-			i++;
-			if (src[i] == '\0')
-			{
-				*(dest + i) = src[i];
-			}
-		}
-	} while (src[i] != '\0');
-
-	return (dest);
-}
-
-
 /**
  * print_all - a function that prints anything, followed by a new line
  *
@@ -55,8 +14,6 @@ void print_all(const char * const format, ...)
 {
 	va_list my_variables;
 	int count;
-	char s1[98];
-	char *ptr;
 
 	va_start(my_variables, format);
 
@@ -80,11 +37,7 @@ void print_all(const char * const format, ...)
 		}
 		if (format[count] == 's')
 		{
-			ptr = _strcpy(s1, va_arg(my_variables, char*));
-			if (s1[0] == '\0')
-				printf("nil");
-			else
-				printf("%s", ptr);
+			printf("%s", va_arg(my_variables, char *));
 			printf(", ");
 		}
 		count++;
