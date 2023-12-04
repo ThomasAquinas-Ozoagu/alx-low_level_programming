d510e7521f957ff324a912ccdda2a6b8cc64bab7
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-12-04 08:39:35 +0000

    correct 4 failed outputs and 1 requirement

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,9 +6,7 @@
 /**
  * read_textfile - reads a file & prints it to the POSIX standard output.
  * @filename: The file to be read
- *
  * @letters: the number of characters to be read and printed
- *
  * Return: the number of characters successfully read
  */
 
@@ -18,14 +16,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int qtyread, qtywrite;
 	char *buf;
 
-/*	printf("About to read: %ld\n", letters); */
-/* Confirm that a file with content was provided */
-	if (filename == NULL)
+	if (filename == NULL) /*Confirm that a file with content was provided*/
 	{
 		printf("filename = NULL");
 		return (0);
 	}
-
 /* create space to store the letters */
 	buf = malloc(letters * sizeof(char));
 	if (!buf)
@@ -33,29 +28,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		printf("Malloc failed\n");
 		return (-1); /* Check if creating the space worked*/
 	}
-
-/* Open the file */
-	fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY); /* Open the file */
 	if (fd == -1)
 		return (0);
-
-/* Proceed to read specified number of letters from the file*/
 	qtyread = read(fd, buf, letters);
 	if (qtyread == -1)
 		return (0);
-/*	printf("Successfully read: %d\n", qtyread); */
-
-
 	qtywrite = write(STDOUT_FILENO, buf, qtyread);
-/*	printf("\nSuccessfully wrote: %d\n", qtywrite); */
 	if (qtywrite !=  qtyread)
 	{
 		printf("\n******************\nWrite failed\n");
 		return (0);
 	}
-
-
 	free(buf); /* Deallocate the temp storage*/
-
 	return (qtywrite); /* Return quantity read*/
 }
