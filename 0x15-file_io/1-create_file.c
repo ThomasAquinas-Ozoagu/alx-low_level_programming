737ce18c09213b6bf8366fd223ac24a75b30ce68
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-12-04 13:02:24 +0000

    Set file permission while creating it

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+/* #include <stdlib.h> */
+/* #include <sys/types.h> */
+/* #include <string.h> */
+
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: name of file to be created
+ *
+ * @text_content: NULL terminated string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ * (file can not be created, file can not be written,
+ * write “fails”, etc…)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, outcome;
+	int nbyte = sizeof(text_content);
+
+/*	printf("File name is %s\n", filename); */
+	if (filename == NULL || text_content == NULL)
+		return (-1); /* Check if file with content was provided*/
+
+/*	printf("Before OPEN, fd = %d\n", fd); */
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600); /*  Open file */
+/*	printf("After OPEN, fd = %d\n", fd); */
+
+	outcome = write(fd, text_content, nbyte);
+	if (outcome == -1)
+		return (-1);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,10 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif /* _READ_TEXTFILE_ */
 
-
-
+#ifndef _CREATE_FILE_
+int create_file(const char *filename, char *text_content);
+#define _CREATE_FILE_
+#endif /* _CREATE_FILE_ */
 
 
 
