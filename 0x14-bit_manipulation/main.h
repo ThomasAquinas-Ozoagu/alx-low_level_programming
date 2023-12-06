64c18d3abf50cda7ebc759dd7840956ff8a39819
ThomasAquinas <ozoagu.benedict@gmail.com>
2023-12-06 22:31:57 +0000

    understand the task first

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -21,6 +21,10 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 #endif /* _GET_BIT_ */
 
+#ifndef _SET_BIT_
+#define _SET_BIT_
+int set_bit(unsigned long int *n, unsigned int index);
+#endif /* _SET_BIT_ */
 
 
 
