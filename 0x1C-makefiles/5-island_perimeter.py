1453fdef571264ae853b36c8f4b9d7934eee8a18
ThomasAquinas <ozoagu.benedict@gmail.com>
2024-08-19 17:25:25 +0000

    whiteboard first, do not try to do everyting in your head

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A function def island_perimeter(grid): that returns the perimeter
+of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            peri = 0
+            if (grid[i][j] == 1):
+                if (grid[i-1][j] == 0):
+                    peri += 1
+                if (grid[i][j-1] == 0):
+                    peri += 1
+                if (grid[i+1][j] == 0):
+                    peri += 1
+                if (grid[i][j+1] == 0):
+                    peri += 1
+                perimeter += peri
+
+    return perimeter
