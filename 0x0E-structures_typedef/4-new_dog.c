d50d06faa537ced2913923055921efe5d17bb9f2
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2024-09-15 16:21:23 +0100

    apply Betty style

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,15 +18,15 @@
 dog_t *new_dog(char *name, float age, char *owner)
 
 {
-  dog_t *dog1;
+	dog_t *dog1;
 
-  dog1 = malloc(sizeof(dog_t));
-  if (dog1 == NULL)
-    return (NULL);
+	dog1 = malloc(sizeof(dog_t));
+	if (dog1 == NULL)
+		return (NULL);
 
-  dog1->name = name;
-  dog1->age = age;
-  dog1->owner = owner;
+	dog1->name = name;
+	dog1->age = age;
+	dog1->owner = owner;
 
-  return dog1;
+	return (dog1);
 }
