49f45700b5aeec37c6c91363f73c7857c2e27390
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2024-09-21 11:49:18 +0100

    replace printf with _putchar

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -12,9 +13,11 @@
  *
  */
 
+
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+	int val = 5;
 	const listint_t *trav = h;
 
 	if (h == NULL)
@@ -22,10 +25,23 @@ size_t print_listint(const listint_t *h)
 
 	while (trav != NULL)
 	{
-		printf("%d\n", trav->n);
+		val = trav->n;
+		_putchar('0'+val);
+		_putchar('\n');
 		count += 1;
 		trav = trav->next;
 	}
-
 	return (count);
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
 }
