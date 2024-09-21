41879ab0c952cf91ab42c057756104e744f90641
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2024-09-21 11:55:21 +0100

    treat _putchar as a separate function

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -33,15 +33,3 @@ size_t print_listint(const listint_t *h)
 	}
 	return (count);
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
