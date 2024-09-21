9bd8fd55f0f11db55414e34b9038c8b13c4e5040
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2024-09-21 14:59:38 +0100

    bettywised

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,7 +4,8 @@
 #include "lists.h"
 
 /**
- * add_nodeint - a function that adds a new node at the beginning of a listint_t list
+ * add_nodeint - a function that adds a new node at
+ * the beginning of a listint_t list
  *
  * @head: the head of the list
  * @n: the integer content of the list
