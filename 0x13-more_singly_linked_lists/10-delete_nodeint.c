823280d79fd63f72c34c8619e939beb447436845
ThomasAquinas Ozoagu <ozoagu.benedict@gmail.com>
2024-12-16 09:16:44 +0100

    listen to Betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,6 +30,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		present = trav->next;
 		free(trav);
 		*head = present;
+		if (*head == NULL)
+			free(head);
 		return (1);
 	}
 	
